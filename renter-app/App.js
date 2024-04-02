4497a9dfa93eabea13f20b79d9a4d6c03e218168
gaurav-afk <gauravcreator123@gmail.com>
2024-04-01 20:04:21 -0400

    commiting to take pull

--- renter-app/App.js
+++ renter-app/App.js
@@ -1,14 +1,26 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, View, Text, TouchableOpacity } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import MapScreen from './View/map';
+import BookingsScreen from './View/bookings';
 
-export default function App() {
+
+const Tab = createBottomTabNavigator();
+
+function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    
+    <NavigationContainer>
+      <Tab.Navigator initialRouteName="Map">
+        <Tab.Screen name="Map" component={MapScreen} />
+        <Tab.Screen name="Bookings" component={BookingsScreen} />
+      </Tab.Navigator>
+    </NavigationContainer>
+
   );
 }
+
 
 const styles = StyleSheet.create({
   container: {
@@ -18,3 +30,43 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
+export default App;
+
+
+
+
+
+
+
+
+
+
+
+
+// import React from 'react';
+// import { View, Text, StyleSheet, Dimensions } from 'react-native';
+// import { TouchableOpacity } from 'react-native-gesture-handler';
+// import { GestureHandlerRootView } from 'react-native-gesture-handler';
+
+
+
+// const BottomSheet = ({car}) => {
+
+//   const bookButton = () => {
+
+//   }
+
+//   return (
+//     <GestureHandlerRootView> {/* Wrap your component hierarchy with GestureHandlerRootView */}
+//     <View style={styles.container}>
+//       <Text style={styles.text}>This is a basic bottom sheet</Text>
+//       {car && <Text style={styles.text}>{car.name}</Text>}
+//       {car && <Text style={styles.text}>Price: ${car.price}</Text>}
+//       <TouchableOpacity onPress={bookButton} style={styles.bookButton}>
+//         <Text style={styles.text}>Book Now</Text>
+//       </TouchableOpacity>
+//     </View>
+//     </GestureHandlerRootView>
+//   );
+// };

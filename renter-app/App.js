55ed28ada195faffb92a22435877a4aa04209a72
laichunyin <laichunyin@gmail.com>
2024-04-03 17:16:43 -0400

    added signin page

--- renter-app/App.js
+++ renter-app/App.js
@@ -1,38 +1,59 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { StyleSheet, View, Text, TouchableOpacity } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import MapScreen from './View/map';
 import BookingsScreen from './View/bookings';
 import Icon from 'react-native-vector-icons/FontAwesome';
+import SignInScreen from './View/SignInScreen';
+import { onAuthStateChanged, signOut } from "firebase/auth"
+import { auth } from "./firebaseConfig"
 
 
 const Tab = createBottomTabNavigator();
 
 function App() {
-  return (
-    
-    <NavigationContainer>
-      <Tab.Navigator initialRouteName="Map"         screenOptions={({ route }) => ({
-          tabBarIcon: ({ focused, color, size }) => {
-            let iconName;
+  const [currentUser, setCurrentUser] = useState()
 
-            if (route.name === 'Map') {
-              iconName = 'map-marker';
-            } else if (route.name === 'Bookings') {
-              iconName = 'bookmark'
-            }
-            return <Icon name={iconName} size={size} color={color} />;
-          },
-        })}
-        tabBarOptions={{
-          activeTintColor: 'tomato',
-          inactiveTintColor: 'gray',
-        }}>
-        <Tab.Screen name="Map" component={MapScreen}/>
-        <Tab.Screen name="Bookings" component={BookingsScreen} />
-      </Tab.Navigator>
-    </NavigationContainer>
+  useEffect(() => {
+    const unsubscribeToUserDataChanges = onAuthStateChanged(auth, (user) => {
+      console.log("user is ", user)
+      if (user) {
+        setCurrentUser(user)
+      } else {
+        setCurrentUser(null);
+      }
+    });
+
+    return unsubscribeToUserDataChanges
+  })
+
+  return (
+    currentUser ? (
+      <NavigationContainer>
+        <Tab.Navigator initialRouteName="Map"         screenOptions={({ route }) => ({
+            tabBarIcon: ({ focused, color, size }) => {
+              let iconName;
+
+              if (route.name === 'Map') {
+                iconName = 'map-marker';
+              } else if (route.name === 'Bookings') {
+                iconName = 'bookmark'
+              }
+              return <Icon name={iconName} size={size} color={color} />;
+            },
+          })}
+          tabBarOptions={{
+            activeTintColor: 'tomato',
+            inactiveTintColor: 'gray',
+          }}>
+          <Tab.Screen name="Map" component={MapScreen}/>
+          <Tab.Screen name="Bookings" component={BookingsScreen} />
+        </Tab.Navigator>
+      </NavigationContainer>
+    )
+    :
+    <SignInScreen />
 
   );
 }
---
+++ renter-app/View/SignInScreen.js
@@ -0,0 +1,69 @@
+import { useState } from "react"
+import { StyleSheet, Text, TextInput, Pressable, View } from "react-native"
+import { auth, db } from '../firebaseConfig';
+import { signInWithEmailAndPassword } from 'firebase/auth';
+import { doc, getDoc } from "firebase/firestore";
+
+const SignInScreen = () => {
+    const [username, setUserName] = useState()
+    const [password, setPassword] = useState()
+
+    const signIn = async () => {
+        try {
+            const docRef = doc(db, "Renters", username)
+            const docSnap = await getDoc(docRef)
+
+            if (docSnap.exists()) {
+                console.log("is renter")
+                await signInWithEmailAndPassword(auth, username, password)
+                console.log("signin success: ", auth.currentUser)
+            }
+            else {
+                console.log("not a renter")
+                alert("Invalid credentials")
+            }
+
+        }
+        catch(err) {
+            console.log("sign in error: ", err)
+            alert("Sign in failed")
+        }
+
+    }
+
+    return (
+        <View style={styles.container}>
+            <Text>Renter App - Sign In</Text>
+
+            <Text>Username: </Text>
+            <TextInput
+                placeholder = "Enter User Name"
+                value = {username}
+                onChangeText = {setUserName}
+            />
+
+            <Text>Password: </Text>
+            <TextInput
+                placeholder = "Enter Password"
+                value = {password}
+                onChangeText = {setPassword}
+            />
+
+            <Pressable onPress={signIn}>
+                <Text>Sign In</Text>
+            </Pressable>
+        </View>
+
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#fff',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+  });
+
+export default SignInScreen
